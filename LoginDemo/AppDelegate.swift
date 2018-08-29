495db525f84aa0885d09a6f8de9acf110b285029
Rio Rinaldi <>
2018-08-29 10:45:43 +0700

    First Upload

--- LoginDemo/AppDelegate.swift
+++ LoginDemo/AppDelegate.swift
@@ -7,15 +7,49 @@
 //
 
 import UIKit
+import Firebase
+import UserNotifications
+
+enum ContentType: String {
+    case movie = "movie"
+    case news = "news"
+}
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    // MARK: - Lifecycle
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Use Firebase library to configure APIs
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+        }
+        else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        
+        if let sessionId = UserDefaults.standard.value(forKey: kSessionIdKey) as? String, !sessionId.isEmpty {
+            showMainViewController()
+        }
+        
         return true
     }
 
@@ -41,6 +75,123 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        let urlString = url.absoluteString
+        if urlString.contains("dymovies://") {
+            if let pathString = urlString.components(separatedBy: "dymovies://").last {
+                let data = pathString.components(separatedBy: "/")
+                let type = data[0]
+                let id = data[1]
+                
+                let dict = [
+                    "type": type,
+                    "id": id
+                ]
+                
+                handlePushNotification(with: dict)
+            }
+        }
+        
+        return true
+    }
+    
+    // MARK: - Helpers
+    
+    
+    func setRootViewController(_ viewController: UIViewController) {
+        
+        UIView.transition(with: window!, duration: 0.3, options: [.transitionCrossDissolve, .allowAnimatedContent], animations: { () -> Void in
+            
+            let oldState = UIView.areAnimationsEnabled
+            
+            UIView.setAnimationsEnabled(false)
+            self.window?.rootViewController = viewController
+            UIView.setAnimationsEnabled(oldState)
+            
+        }, completion: nil)
+    }
+    
+    func showMainViewController() {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "rootHome")
+        
+        setRootViewController(vc)
+    }
+    
+    func showLoginViewController() {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "rootLogin")
+        
+        setRootViewController(vc)
+    }
+    
+    func handlePushNotification(with userInfo: [AnyHashable: Any]) {
+        
+        if let type = userInfo["type"] as? String, let contentType = ContentType(rawValue: type) {
+            
+            switch contentType {
+            case .movie:
+                if let idString = userInfo["id"] as? String, let movieId = Int(idString) {
+                 
+                    if UIApplication.shared.applicationState != .active {
+                        
+                        if let nc = window?.rootViewController as? UINavigationController {
+                            nc.viewControllers.last?.showMovieViewController(with: movieId)
+                        }
+                        else {
+                            
+                            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                            let vc = storyboard.instantiateViewController(withIdentifier: "movie") as! MovieViewController
+                            vc.movieId = movieId
+                            
+                            let nc = UINavigationController(rootViewController: vc)
+                            window?.rootViewController?.present(nc, animated: true, completion: nil)
+                        }
+                    }
+                }
+                
+            case .news:
+                break
+            }
+            
+        }
+    }
+    
+    // MARK: - Notifications
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        if application.applicationState == .inactive {
+            handlePushNotification(with: userInfo)
+        }
+        completionHandler(.newData)
+    }
 }
+
+// MARK: - UNUserNotificationCenterDelegate
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        let userInfo = response.notification.request.content.userInfo
+        handlePushNotification(with: userInfo)
+        completionHandler()
+    }
+}
+
+// MARK: - MessagingDelegate
+extension AppDelegate: MessagingDelegate {
+
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        print("*** FCM toke: \(fcmToken)")
+    }
+}
+
+
+
+
+
 
